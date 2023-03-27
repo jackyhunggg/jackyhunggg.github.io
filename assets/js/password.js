94c971961f8916fad9155fdd9ae9aa5e62a296a3
Jacky Hung <jacky85725@gmail.com>
2023-03-27 18:01:47 +0800

    fix password.js

--- assets/js/password.js
+++ assets/js/password.js
@@ -5,54 +5,6 @@ function sample(array) {
 }
 
 // define the password generator function
-function generatePassword(options) {
-// define things user might want
-const lower = 'abcdefghijklmnopqrstuvwxyz'
-const upper = lower.toUpperCase()
-const num = '1234567890'
-const symbol = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
-
-// create a collection to store things user picked up
-let collection = []
-
-if(options.lowercase === 'on') {
-    collection = collection.concat(lower.split(''))
+function generatePassword() {
+    window.alert('Psyche! This website cannot run backend shit!(as far as I know)')
 }
-
-if(options.uppercase === 'on') {
-    collection = collection.concat(upper.split(''))
-}
-
-if(options.numbers === 'on') {
-    collection = collection.concat(num.split(''))
-}
-
-if(options.symbols === 'on') {
-    collection = collection.concat(symbol.split(''))
-}
-// remove things user do not need
-if(options.excludeCharacters) {
-    collection = collection.filter(character => {
-      return !options.excludeCharacters.includes(character)
-      })      
-}
-
-// return error notice if collection is empty
-if (collection.length === 0) {
-    return 'There is no valid character in your selection.'
-  }
-
-// start generating password
-// make a empty string for password
-let password = ''
-
-for(let i = 0; i < Number(options.length); i ++) {
-    password += sample(collection)
-}
-
-// return the generated password
-return password
-}
-
-// export generatePassword function for other files to use
-module.exports = generatePassword
